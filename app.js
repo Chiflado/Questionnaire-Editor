a302d741ac03458e436157156f53d807cc5a4697
Chiflado <prevarabalazs@gmail.com>
2020-01-27 14:35:00 +0100

    Lists questions

--- app.js
+++ app.js
@@ -12,8 +12,28 @@ function ajaxCall(command, callback){
     xhr.send();
 };
 
+function createList(data){
+    let list = document.getElementsByClassName('question-list');
+    console.log(list);
+    // document.querySelector('body').appendChild(list[0]);
+    for (let i = 0; i<data.length; i++){
+        let postElements =`<div class="question-container">
+                                <span class="question">${data[i].question}</span>
+                                <span class="date">(${moment.unix(data[i].created_at).format("YYYY/MM/DD HH:mm")})</span>
+                            </div>
+                            <div class="answer">
+                                ${data[i].answer}
+                            </div>`;
+        let bookDatas = document.createElement('li');
+        bookDatas.className = 'question-list-element';
+        bookDatas.innerHTML = postElements;
+        list[0].appendChild(bookDatas);
+    }
+}
+
 function handleData(data){
     console.log(data);
 }
 
 ajaxCall('GET', handleData);
+ajaxCall('GET', createList);
