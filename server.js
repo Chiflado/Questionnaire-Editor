57b71b33ff2f81e7da9b5f342eb69ba2e9a13711
Chiflado <prevarabalazs@gmail.com>
2020-01-27 14:06:28 +0100

    Adds server.js to host local

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+
+var port = 3000;
+
+var app = express();
+express.json.type = "application/json";
+app.use(express.json());
+app.use('/', express.static('./'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port);
+console.log('the server run at: http://localhost:' + port);
